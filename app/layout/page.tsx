fa6000f8295979d924c4160c85407ddb904a7fe8
miraj-brandhub <miraj@brandhub.io>
2026-01-15 12:51:22 +0530

    added new theme

--- app/layout/page.tsx
+++ app/layout/page.tsx
@@ -1,5 +1,5 @@
-import { DataTableDemo } from "@/registry/metis/blocks/data-table/data-table";
-import MainLayout from "@/registry/metis/Layout/MainLayout";
+import { DataTableDemo } from "@/registry/new-york/blocks/data-table/data-table";
+import MainLayout from "@/registry/new-york/Layout/MainLayout";
 import React from "react";
 
 function Page() {
